0a1d98141c7aec45e83ad40e538c1ca84ae44fca
Kenny Cen <kennycen1976@gmail.com>
2024-09-13 21:39:28 -0400

    added final pictures for team section

--- ucla-website/src/components/Team Section/Team.jsx
+++ ucla-website/src/components/Team Section/Team.jsx
@@ -11,13 +11,13 @@ const Team = () => {
             <ImageCont src='anthony.png' title='👨‍💼 President 👨‍💼' name='Anthony Zhang'/>
             <ImageCont src='michael.png' title='👔 Vice President 👔' name='Michael Jiang'/>
             <ImageCont src='jacky.png' title='💰 Treasurer 💰' name='Jacky Mei'/>
-            <ImageCont src='nap5.jpg' title='📋 Secretary 📋' name='Jamie Wu'/>
+            <ImageCont src='jamie.png' title='📋 Secretary 📋' name='Jamie Wu'/>
         </div>
         <h2>Board</h2>
         <div className="board-container">
             <ImageCont src='jessie.png' title='🏞️ Graphics 🏞️' name='Jessie Tam'/>
-            <ImageCont src='nap5.jpg' title='🏞️ Graphics 🏞️' name='Victoria Sung'/>
-            <ImageCont src='nap5.jpg' title='🏞️ Graphics 🏞️' name='Tiffany Lung'/>
+            <ImageCont src='victoria.png' title='🏞️ Graphics 🏞️' name='Victoria Sung'/>
+            <ImageCont src='tiffany.png' title='🏞️ Graphics 🏞️' name='Tiffany Lung'/>
             <ImageCont src='anna.png' title='🪪 Membership 🪪' name='Anna Deng'/>
             <ImageCont src='esha.png' title='🪪 Membership 🪪' name='Puspita Esha'/>
             <ImageCont src='kaylin.png' title='🫂 Philanthropy 🫂' name='Kaylin Zhou'/>
@@ -32,7 +32,7 @@ const Team = () => {
             <ImageCont src='ming.png' title='⛩️ CLP ⛩️' name='Mingshuo Zhang'/>
             <ImageCont src='peiling.png' title='🖊️ Assist. Treasurer 🖊️' name='Peiling Luo'/>
             <ImageCont src='johnson.png' title='🖊️ Assist. Treasurer 🖊️' name='Johnson Guo'/>
-            <ImageCont src='nap5.jpg' title='🤝 Public Relations 🤝' name='Anson Wat'/>
+            <ImageCont src='anson.png' title='🤝 Public Relations 🤝' name='Anson Wat'/>
             <ImageCont src='fiona.png' title='🎏 Events 🎏' name='Fiona Cheng'/>
             <ImageCont src='bryan.png' title='🎏 Events 🎏' name='Bryan Ang'/>
         </div>
